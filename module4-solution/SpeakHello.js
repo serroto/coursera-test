b782100c83cdc5b20c3bbc34fd63e5e7a812bc9c
serroto <71097293+serroto@users.noreply.github.com>
2020-09-29 16:40:23 +0300

    Add files via upload
    Simple JS practice.

---
+++ module4-solution/SpeakHello.js
@@ -0,0 +1,10 @@
+
+(function(window) {
+  var helloSpeaker ={
+      speak: function (name) {
+      console.log(speakWord + " " + name);
+  }
+};
+var speakWord = "Hello";
+window.helloSpeaker=helloSpeaker;
+})(window);
